522f6c21a32d0590a2a752f1b13ffc94646c4254
Роман Фисенко <1roman.fis@gmail.com>
2026-03-13 22:35:42 +0700

    Задание 1.4

---
+++ lab-2/lab_1_4.py
@@ -0,0 +1,20 @@
+sum_numbers = 0
+count = 0
+
+print("Введите целые числа. Для завершения введите stop.")
+
+while True:
+    user_input = input("Введите число: ")
+    if user_input.lower() == "stop":
+        break
+    try:
+        num = int(user_input)
+    except ValueError:
+        print("Ошибка! Введите целое число.")
+        continue
+
+    sum_numbers = sum_numbers + num
+    count = count + 1
+
+print("Сумма чисел:", sum_numbers)
+print("Количество чисел:", count)
